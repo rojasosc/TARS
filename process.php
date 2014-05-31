911b104ff4bc83fefe66b337c5484d3629aa013e
root <root@localhost.localdomain>
2014-05-31 17:57:00 -0400

    Updated the reference to the new database interface.

--- process.php
+++ process.php
@@ -1,5 +1,5 @@
 <?php
-	include ("dbinterface.php");
+	include ("db.php");
 	
 	registerStudent($_POST['firstName'],$_POST['lastName'],$_POST['email'],$_POST['password'],$_POST['homePhone'],$_POST['mobilePhone'],$_POST['classYear'],$_POST['major'],$_POST['gpa'],$_POST['aboutMe']);
 

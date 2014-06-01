f9f8f8b93698872ecaeef73b15a72a44fdbceb5d
raign <jan2@u.rochester.edu>
2014-06-01 15:37:39 -0400

    started staff folder
    currently have:
    basic staff home page
    staffSession.php
    staff.css that's a carbon copy of student.css with a different name

---
+++ staff/staffSession.php
@@ -0,0 +1,17 @@
+<?php
+
+	include('../db.php');
+	
+	session_start();
+	session_regenerate_id(true);
+	
+	if(!isset($_SESSION['auth'])){
+		header('Location: ../index.php');
+	} else {
+		$email = $_SESSION['email'];	//Extract the email from the session to fetch a staff object
+		$staff = getStaff($email);		//Fetch said staff object
+		$fn = $staff['firstName'];	//Fetch the first name
+		$ln = $staff['lastName'];		//Fetch the last name
+		$brand = $fn[0].". ".$ln;		//Create a single variable to hold the brand at the far left of the navbar
+	}
+?>

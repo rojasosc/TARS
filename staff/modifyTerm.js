823fd2423874bb87c5c8f2822e8cffe2a3f27364
Oscar Rojas <orojas@u.rochester.edu>
2014-06-07 20:32:11 -0400

    Developed the UI for editing/creating new terms as well as editing/creating courses and positions.

--- staff/modifyTerm.js
+++ staff/modifyTerm.js
@@ -1,13 +1,92 @@
 $(function(){
 
+	/* obtain relevant component references (jQuery Objects) */
+	
+	$term = $('#selectTerm'); /* term drop table */
+	
+	$updateCourseContainer = $('#updateCourseContainer');
+	
+	
+	$course = $('#selectCourse'); /* course drop table */
+	
+	$professor = $('#selectProfessor'); /* professor drop table */
+		
 	/* bind components */
 	
-	$('#selectTerm').bind('change',displayTerm);
+	$term.bind('change',displayTerm);
+	$course.bind('change',showCourse);
+	$course.bind('change',showProfessors);
+	$professor.bind('change',showCourse);
+	
+	/* prevent a page redirection */
+	
+	$('#updateCourseForm').submit(function (){
+		
+		return false;
+	});
+	
+	$('#newCourseForm').submit(function (){
+		
+		return false;
+	});
+
+	
+	$('#updateCourseButton').bind('click',updateCourse);
+	$('#newCourseButton').bind('click',newCourse);
 	
 	showTerm();
 	
 }); /* End of onload event*/
 
+
+
+function updateCourse(){
+	
+	
+	
+}
+
+function newCourse(){
+	
+	
+	
+}
+
+function showCourse(){
+	
+	/*hide the current course form */
+	
+	$updateCourseContainer.fadeOut(200);
+	
+	/*obtain the course via ajax get request */
+	
+	
+	$updateCourseContainer.fadeIn(200);
+	
+}
+
+
+
+function showProfessors(){
+	
+	/* remove current professor drop table */
+	$professor.prev().hide();
+	$professor.hide();
+	
+	//hideCourseForm(); /* in case a course form was already opened */
+	
+	/* show the professors once the course has been selected */
+	
+	/* AJAX get request to retrieve professors who teach this course */
+	
+	
+	/*build options based on the results obtained */
+	$professor.prev().show(200);
+	$professor.show(300); /* display professors retrieved from ajax get request */
+	
+	
+	
+}
 
 function displayTerm(){
 	
@@ -33,7 +112,7 @@ function hideTerm(){
 }
 
 function showTerm(){
-	var termHeader = $('#selectTerm').val();
+	var termHeader = $term.val();
 	$('.termHeader').text(termHeader);
 	$('#termOverview').fadeIn(500);
 }
--- staff/modifyTerm.php
+++ staff/modifyTerm.php
@@ -76,7 +76,9 @@
 	  
 			<!-- BEGIN Page Content -->
 			<div id="content">
-			
+				<div class="page-header">
+					<h1>Modify Term</h1>
+				</div>			
 				<div class="container">
 					<form action="#" class="form-horizontal" id="newTermForm" method="post">		
 						<div class="row">		
@@ -97,59 +99,250 @@
 				</div> <!-- End container -->
 				
 				<div class="container" id="termContainer">
-										<!-- Nav tabs -->
+						<!-- Nav tabs -->
 					<ul class="nav nav-tabs">
 						<li class="active"><a href="#term" data-toggle="tab">Term Overview</a></li>
 						<li><a href="#manageCourses" data-toggle="tab">Manage Courses</a></li>
 						<li><a href="#managePositions" data-toggle="tab">Manage Positions</a></li>
-						<li><a href="#manageLocations" data-toggle="tab">Manage Locations</a></li>
-					</ul>
-
+					</ul> <!-- End Nav tabs -->
 					<!-- Tab panes -->
-					<div class="tab-content">	
+					<br>
+					<div class="tab-content">
+						
+						<!-- Begin Term Overview Pane -->
 						<div class="tab-pane fade in active" id="term">
 							<br>		
 							<div class="container" id="termOverview">
 								<h3 class="termHeader"></h3>
 								<h4>Number of Courses: (Number)</h4>
 								<h4>Number of Positions: (Number)</h4>
-								<h4>Number of Locations: (Number)</h4>	
+								<h4>Number of Locations: (Number)</h4>
+								<h4>Last Edited: (date)</h4>	
 							</div> <!-- End container -->
 						</div> <!-- end tab-pane -->
-						<div class="tab-pane fade" id="manageCourses">
-							<div class="container">
-								<br>
-								<h3 class="termHeader"></h3>
-								<div class="row">
-									<div class="col-md-4">
-										<div class="form-group">
-											<label class="control-label" for="major">Select Course</label>
-											<select name="major" class="form-control" placeholder="Major">
-												<option>The Science of Data Structures</option>
-												<option>Computation and Formal Systems</option>
-												<option>Computer Organization</option>
-												<option>Web Programming</option>
-											</select> <!-- End select -->										
-										</div> <!-- End form-group -->
-									</div> <!-- End column -->
-									<div class="col-md-4">
-										<div class="form-group">
-											<label class="control-label" for="major">Select Professor</label>
-											<select name="major" class="form-control" placeholder="Major">
-												<option>Ted Pawlicki</option>
-												<option>Chris Brown</option>
-												<option>Michael Scott</option>
-											</select> <!-- End select -->										
-										</div> <!-- End form-group -->
-									</div> <!-- End column -->									
-								</div> <!-- End row -->							
-							</div> <!-- end container -->
-						</div> <!-- end tab-pane -->
+						<!-- End Term Overview Pane -->
+						
+						
+						<div class="tab-pane fade in" id="manageCourses">						
+							<!-- Nav tabs -->
+							<ul class="nav nav-pills">
+								<li class="active"><a href="#newCourse" data-toggle="tab">New Course</a></li>
+								<li><a href="#editCourse" data-toggle="tab">Modify Course</a></li>
+							</ul> <!-- End Nav tabs -->
+							<h3 class="termHeader"></h3>
+							<div class="tab-content">
+								<div class="tab-pane fade in active" id="newCourse">
+									
+									<form class="form-horizontal" method="post" action="#" id="newCourseForm">
+										<legend>Create Course</legend>
+										<div class="row">
+											<div class="col-md-4">
+												<label class="control-label" for="courseNumber">Course Number</label>
+												<input type="text" name="courseNumber" placeholder="Course Number" class="form-control">
+											</div> <!-- End column -->
+											<div class="col-md-4">
+												<label class="control-label" for="courseTitle">Course Title</label>
+													<input type="text" name="courseTitle" placeholder="Course Title" class="form-control">
+											</div> <!-- End column --> 
+										</div> <!-- End row -->
+										<div class="row">
+											<div class="col-md-4">
+												<label class="control-label" for="startTime">Start Time</label>
+												<input type="time" name="startTime" placeholder="" class="form-control">
+											</div> <!-- End column -->
+											<div class="col-md-4">
+												<label class="control-label" for="endTime">End Time</label>
+												<input type="time" name="endTime" placeholder="" class="form-control">
+											</div> <!-- End column -->
+										</div> <!-- End row -->										
+										<div class="row">
+											<div class="col-md-4">
+												<label class="control-label" for="websiteURL">Website</label>
+												<input type="url" name="websiteURL" placeholder="" class="form-control">
+											</div> <!-- End column -->
+											<div class="col-md-4">						
+												<label class="control-label" for="professor">Professor</label>
+												<select name="professor" class="form-control">
+													<option>Ted Pawlicki</option>
+													<option>Chris Brown</option>
+													<option>Michael Scott</option>
+												</select> <!-- End select -->										
+											</div> <!-- End column -->
+										</div> <!-- End row -->
+										<legend>Location</legend>
+										<div class="row">
+											<div class="col-md-4">											
+												<label class="control-label" for="building">Building</label>
+												<select name="building" class="form-control" placeholder="Building">
+													<option>CSB</option>
+													<option>LATT</option>
+													<option>Meliora</option>
+												</select> <!-- End select -->										
+											</div> <!-- End column -->
+											<div class="col-md-4">													
+												<label class="control-label" for="room">Room</label>
+												<select name="room" class="form-control" placeholder="Room">
+													<option>306</option>
+													<option>233</option>
+													<option>255</option>
+													<option>219</option>
+													<option>393</option>
+												</select> <!-- End select -->																							
+											</div> <!-- End column -->							
+										</div> <!-- End Row -->
+										<br>
+										<div class="row">
+											<div class="col-md-2">
+												<button id="newCourseButton" type="submit"  name="newCourseButton" class="btn btn-success btn-block"><span class="glyphicon glyphicon-plus"></span> Create New Course</button>
+											</div> <!-- End column -->											
+										</div> <!-- End row -->	
+										<br>
+									</form> <!-- End form -->													
+								</div> <!-- End tab-pane new course -->
+								<div class="tab-pane fade" id="editCourse">							
+									<div class="container">
+										<div class="row">
+											<div class="col-md-4">
+												<div class="form-group">
+													<label class="control-label" for="course">Select Course</label>
+													<select name="course" class="form-control" id="selectCourse">
+														<option>The Science of Data Structures</option>
+														<option>Computation and Formal Systems</option>
+														<option>Computer Organization</option>
+														<option>Web Programming</option>
+													</select> <!-- End select -->										
+												</div> <!-- End form-group -->
+											</div> <!-- End column -->
+											<div class="col-md-4" id="professorColumn">
+												<div class="form-group">
+													<label class="control-label" for="professor">Select Professor</label>
+													<select name="professor" class="form-control" id="selectProfessor">
+														<option>Ted Pawlicki</option>
+														<option>Chris Brown</option>
+														<option>Michael Scott</option>
+													</select> <!-- End select -->										
+												</div> <!-- End form-group -->
+											</div> <!-- End column -->									
+										</div> <!-- End row -->	
+									</div> <!-- End container -->	
+										<div class="container" id="updateCourseContainer">
+											<form class="form-horizontal" method="post" action="#" id="updateCourseForm">
+												<legend>Update Course</legend>
+												<div class="row">
+													<div class="col-md-4">
+														<label class="control-label" for="courseNumber">Course Number</label>
+														<input type="text" name="courseNumber" placeholder="Course Number" class="form-control">
+													</div> <!-- End column -->
+													<div class="col-md-4">
+														<label class="control-label" for="courseTitle">Course Title</label>
+														<input type="text" name="courseTitle" placeholder="Course Title" class="form-control">
+													</div> <!-- End column --> 
+												</div> <!-- End row -->
+												<div class="row">
+													<div class="col-md-4">
+														<label class="control-label" for="startTime">Start Time</label>
+														<input type="time" name="startTime" placeholder="" class="form-control">
+													</div> <!-- End column -->
+													<div class="col-md-4">
+														<label class="control-label" for="endTime">End Time</label>
+														<input type="time" name="endTime" placeholder="" class="form-control">
+													</div> <!-- End column -->
+												</div> <!-- End row -->										
+												<div class="row">
+													<div class="col-md-4">
+														<label class="control-label" for="websiteURL">Website</label>
+														<input type="url" name="websiteURL" placeholder="" class="form-control">
+													</div> <!-- End column -->
+													<div class="col-md-4">
+														
+															<label class="control-label" for="professor">Professor</label>
+															<select name="professor" class="form-control">
+																<option>Ted Pawlicki</option>
+																<option>Chris Brown</option>
+																<option>Michael Scott</option>
+															</select> <!-- End select -->										
+														
+													</div> <!-- End column -->
+												</div> <!-- End row -->
+												<legend>Location</legend>
+												<div class="row">
+													<div class="col-md-4">
+														
+															<label class="control-label" for="building">Building</label>
+															<select name="building" class="form-control" placeholder="Building">
+																<option>CSB</option>
+																<option>LATT</option>
+																<option>Meliora</option>
+															</select> <!-- End select -->										
+														
+													</div> <!-- End column -->
+													<div class="col-md-4">
+														
+															<label class="control-label" for="room">Room</label>
+															<select name="room" class="form-control" placeholder="Room">
+																<option>306</option>
+																<option>233</option>
+																<option>255</option>
+																<option>219</option>
+																<option>393</option>
+															</select> <!-- End select -->										
+														
+													</div> <!-- End column -->							
+												</div> <!-- End Row -->
+												<br>
+												<div class="row">
+													<div class="col-md-2">
+														<button id="updateCourseButton" type="submit"  name="updateCourseButton" class="btn btn-success btn-block"><span class="glyphicon glyphicon-refresh"></span> Update</button>
+													</div> <!-- End column -->
+													<div class="col-md-2">
+														<button id="removeCourseButton" type="submit"  name="removeCourseButton" class="btn btn-danger btn-block"><span class="glyphicon glyphicon-remove"></span> Remove</button>
+													</div> <!-- End column -->														
+												</div> <!-- End row -->											
+											</form> <!-- End form -->
+											<br>
+										</div> <!-- End container -->													
+								</div> <!-- End tab-pane edit course -->													
+							</div> <!-- End tab-content manage courses -->
+						</div> <!-- End tab-pane manage courses -->
+						
 						<div class="tab-pane fade" id="managePositions">
 							<h3 class="termHeader"></h3>
-						</div> <!-- end tab-pane -->
-						<div class="tab-pane fade" id="manageLocations">
-							<h3 class="termHeader"></h3>
+								<div class="container">
+									<form class="form-horizontal" method="post" action="#">
+										<div class="row">
+											<div class="col-md-4">
+												<div class="form-group">
+													<label class="control-label" for="course">Select Course</label>
+													<select name="course" class="form-control" id="selectCourse">
+														<option>The Science of Data Structures</option>
+														<option>Computation and Formal Systems</option>
+														<option>Computer Organization</option>
+														<option>Web Programming</option>
+													</select> <!-- End select -->										
+												</div> <!-- End form-group -->
+											</div> <!-- End column -->
+											<div class="col-md-4" id="professorColumn">
+												<div class="form-group">
+													<label class="control-label" for="professor">Select Professor</label>
+													<select name="professor" class="form-control" id="selectProfessor">
+														<option>Ted Pawlicki</option>
+														<option>Chris Brown</option>
+														<option>Michael Scott</option>
+													</select> <!-- End select -->										
+												</div> <!-- End form-group -->
+											</div> <!-- End column -->												
+										</div> <!-- End row -->										
+									</form> <!-- End form manage positions -->
+								</div> <!-- end container -->
+								<div class="container">
+									<table class="table table-striped">
+										<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Course</th><th>Type</th> <th>GPA</th><th>View Profile</th></tr>
+										<tr><td>3</td> <td>Jinze</td> <td>Ahn</td> <td>jan2@u.rochester.edu</td><td>CSC171</td><td>Lab TA</td><td>4.00</td><td><a type="button" type="button" data-toggle="modal" href="#myProfile3" class="btn btn-default">
+											<span class="glyphicon glyphicon-user"></span> Profile</a>
+											</td>
+										</tr> 												
+									</table>
+								</div> <!-- End container -->
 						</div> <!-- end tab-pane -->
 					</div>	<!-- end tab-content -->
 				</div> <!-- End container -->

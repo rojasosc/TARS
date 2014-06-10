d9514c0df913aec520d792fd3e1685285b256415
Nate Book <nmbook@gmail.com>
2014-06-09 22:31:52 -0400

    Clean up to sessions.
    Do not session_regenerate_id every page load.
    
    Always end the script after a Location:, so the rest of the page is not
    generated or sent before we are given a new page anyway.
    
    If the user lookup failed (the logged-in user is not of that type or the
    session is invalid), then redirect to index.php.

--- staff/reviewProcess.php
+++ staff/reviewProcess.php
@@ -1,24 +1,22 @@
 <?php
 
-	include('../db.php');
-	 
-	 foreach($_POST as $action){
-		$IDs = explode(" ",$action);
-		
-		$status = $IDs[0];
-		$studentID = $IDs[1];
-		
- 		echo $status . " " . $studentID . "<br>";
-		
-		if(!!$status){
-		
-			setStatus($studentID,$status);
-		}
-		
-		
-	 }
-	 
-	header('Location: verifyStudents.php');
+include('../db.php');
+
+foreach($_POST as $action){
+	$IDs = explode(" ",$action);
+
+	$status = $IDs[0];
+	$studentID = $IDs[1];
+
+	echo $status . " " . $studentID . "<br>";
+
+	if(!!$status){
+
+		setStatus($studentID,$status);
+	}
 
 
-?>
+}
+
+header('Location: verifyStudents.php');
+

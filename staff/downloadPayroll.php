f8459f283a07ea79fbf66807e0c1131b14454c2e
Oscar Rojas <orojas@u.rochester.edu>
2014-06-11 13:25:29 -0400

    Updated the UI xls button and fixed the payroll spreadsheet script.

--- staff/downloadPayroll.php
+++ staff/downloadPayroll.php
@@ -1,18 +1,32 @@
 <?php
+	include("../db.php");
+	$term = CURRENT_TERM;
+	$fileName = "payroll" . CURRENT_TERM . ".xls";
+	$assistants = getPayrollByTerm($term);
+	header("Content-Type: application/vnd.ms-excel");
+	
+	/* Table header */
+	echo 'University ID'. "\t". 'First Name' . "\t" . 'Last Name' . "\t" . 'Email' . "\t" .'CRN' . "\t" . 'Type' . "\t" . 'Class Year' . "\t" . 'Compensation' . "\n";
 
-include("../db.php");
+	/* Insert each position into the spreadsheet */
+	foreach($assistants as $assistant){
+		$student = $assistant->getStudent();
+		$position = $assistant->getPosition();
+		$course = $position->getCourse();
+		
+		/* Column values */
+		$universityID = $student->getUniversityID();
+		$firstName = $student->getFirstName();
+		$lastName = $student->getLastName();
+		$email = $student->getEmail();
+		$crn = $course->getCRN();
+		$type = $position->getPositionType();
+		$classYear = $student->getClassYear();
+		$compensation = $assistant->getCompensation();
+		
+		/* Echo each column value */
+		echo $universityID ."\t". $firstName . "\t" . $lastName . "\t" . $email . "\t" . $crn . "\t" . $type . "\t" . $classYear . "\t" . $compensation ."\n";
 
-$assistants = getPayrollByTerm($term);
-
-
-header("Content-Type: application/vnd.ms-excel");
-echo 'ID'. "\t". 'First Name' . "\t" . 'Last Name' . "\t" . 'Email' . "\t" .'Course' . "\t" . 'Type' . "\t" . 'Class Year' . "\t" . 'Compensation' . "\n";
-foreach($assistants as $assistant){
-
-	echo $assistant['studentID']. "\t". $assistant['firstName'] . "\t" . $assistant['lastName'] . "\t" . $assistant['email'] . "\t" . $assistant['courseNumber'] . "\t" . $assistant['type'] . "\t" . $assistant['classYear'] . "\t" . $assistant['compensation'] ."\n";
-
-}
-
-header("Content-disposition: attachment; filename=spreadsheet.xls");
-
+	}
+	header("Content-disposition: attachment; filename=".$fileName);
 ?>

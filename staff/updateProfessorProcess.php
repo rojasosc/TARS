d30ecc846b7dbf650725fd157447512ab15224e0
Oscar Rojas <orojas@u.rochester.edu>
2014-06-12 16:56:10 -0400

    updated to use new updateProfile(), still needs work

--- staff/updateProfessorProcess.php
+++ staff/updateProfessorProcess.php
@@ -2,6 +2,5 @@
 	include('../db.php');
 	$professor = User::getUserByEmail($_POST['email'],PROFESSOR);
 	$professor->updateProfile($_POST['firstName'],$_POST['lastName'],$POST_['placeID'],$POST_['officePhone'],$_POST['mobilePhone']);	
-	updateProfile($firstName, $lastName, $place, $officePhone, $mobilePhone)
 
 ?>

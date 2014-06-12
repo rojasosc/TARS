fd20408592651523614e9e1346b53a0847773f15
Oscar Rojas <orojas@u.rochester.edu>
2014-06-11 23:38:46 -0400

    Updated to obtain a Student object and update the profile.

--- staff/updateStudentProcess.php
+++ staff/updateStudentProcess.php
@@ -1,8 +1,8 @@
 <?php
-
+	/* There seems to be an error with the updateProfile db.php function */
 	include('../db.php');
+	$student = User::getUserByEmail($_POST['email'],STUDENT);
+	$student->updateProfile($_POST['firstName'],$_POST['lastName'],$_POST['mobilePhone'],$_POST['classYear'],$_POST['major'],$_POST['gpa'],$_POST['aboutMe']);	
 	
-	Student::updateProfile($_POST['firstName'],$_POST['lastName'],$_POST['homePhone'],$_POST['mobilePhone'],$_POST['classYear'],$_POST['major'],$_POST['gpa'],$_POST['aboutMe']);	
-
 
 ?>

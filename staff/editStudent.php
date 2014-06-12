975cd249705988740a6aac23dd1e8e70a7375963
Oscar Rojas <orojas@u.rochester.edu>
2014-06-11 23:30:50 -0400

    Restructured the page to work correctly with the updated scripts.

--- staff/editStudent.php
+++ staff/editStudent.php
@@ -1,8 +1,12 @@
 <?php  
-    ini_set('display_errors',1);	
-    include('staffSession.php');
-?>
+   
+	include('staffSession.php');
+    
+    	ini_set("display_errors",1);
+    	
+    	
 
+?>
 <!DOCTYPE html>
 <html lang="en">
 	<head>
@@ -22,12 +26,12 @@
 	</head>
 	<body>
 		<!-- BEGIN Edit Profile Modal-->
-		<div class="modal fade" id="editProfileID" tabindex="-1" role="dialog" aria-labelledby="editProfileModal" aria-hidden="true">
+		<div class="modal fade" id="editProfileModal" tabindex="-1" role="dialog" aria-labelledby="editProfileModal" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h1 class="modal-title">Edit (name)'s Profile</h1>
+						<h1 class="modal-title" id="modalHeader"></h1>
 					</div> 
 					<div class="modal-body">
 						<form action="updateStudentProcess.php" class="form-horizontal" id="updateForm" method="post">
@@ -35,13 +39,13 @@
 								<div class="col-md-4">				
 									<div class="form-group"> 				
 										<label class="control-label" for="firstName">First Name</label>
-											<input id="firstName" type="text" class="form-control" name="firstName"/>																					
+											<input id="firstName" type="text" class="form-control" name="firstName">																					
 									</div> <!-- End form-group -->											
 								</div> <!-- End column -->
 									<div class="col-md-4">
 										<div class="form-group">
 											<label class="control-label" for="firstName">Last Name</label>
-												<input id="lastName" type="text" class="form-control" name="lastName" />													
+												<input id="lastName" type="text" class="form-control" name="lastName">													
 										</div> <!-- End form-group -->							
 									</div>	<!-- End column -->						
 							</div> <!-- End row -->
@@ -49,64 +53,56 @@
 								<div class="col-md-4">
 									<div class="form-group">
 										<label class="control-label" for="email">Email</label>
-										<input id="email" type="email" class="form-control" name="email"/>					
+										<input id="email" type="email" class="form-control" name="email">					
 									</div> <!-- End form-group -->							
 								</div>	<!-- End column -->						
-							</div> <!-- End row -->
-							<div class="row">
-								<div class="col-md-4">
-									<div class="form-group">
-										<label class="control-label" for="homePhone">Home Phone</label>
-										<input id="homePhone" type="tel" class="form-control" name="homePhone" placeholder="Home Phone"/>
-									</div> <!-- End form-group -->
-								</div> <!-- End column -->
 								<div class="col-md-4">
 									<div class="form-group">
 										<label class="control-label" for="mobilePhone">Mobile Phone</label>
-										<input id="mobilePhone" type="tel" class="form-control" name="mobilePhone" placeholder="Mobile Phone"/>
+										<input id="mobilePhone" type="tel" class="form-control" name="mobilePhone" placeholder="Mobile Phone">
 									</div> <!-- End form-group -->
 								</div> <!-- End column -->								
 							</div> <!-- End row -->
 						<legend>Academic Information</legend>
-						<div class="row">
-							<div class="col-md-4">
-								<div class="form-group">
-									<label class="control-label" for="classYear">Class Year</label>
-									<select id="classYear" name="classYear" class="form-control" placeholder="Class Year">
-										<option>2014</option>
-										<option>2015</option>
-										<option>2016</option>
-										<option>2017</option>
-										<option>2018</option>
-									</select> <!-- End select -->										
-								</div> <!-- End form-group -->
-							</div> <!-- End column -->
-							<div class="col-md-4">
-								<div class="form-group">
-									<label class="control-label" for="major">Major</label>
-									<select id="major" name="major" class="form-control" placeholder="Major">
-										<option>Accounting</option>
-										<option>Computer Science</option>
-										<option>Physics</option>
-										<option>Mathematics</option>
-										<option>Economics</option>
-									</select> <!-- End select -->										
-								</div> <!-- End form-group -->
-							</div> <!-- End column -->							
-						</div> <!-- End Row -->	
-						<div class="row">
-							<div class="col-md-4">
-								<div class="form-group">
-									<label class="control-label" for="GPA">GPA</label>
-									<input id="gpa" type="text" class="form-control" name="gpa" placeholder="GPA"/>
-								</div> <!-- End form-group -->
-							</div> <!-- End column -->								
-						</div> <!-- End row -->	
-						<div class="row">
-							<div class="col-md-8">
-								<label class="control-label" for="aboutMe">About Me</label>
-								<textarea id="aboutMe" class="form-control" name="aboutMe" placeholder="Fill this area with previous experience and relevant qualifications."></textarea>
-						</div> <!-- End row -->					
+							<div class="row">
+								<div class="col-md-4">
+									<div class="form-group">
+										<label class="control-label" for="classYear">Class Year</label>
+										<select id="classYear" name="classYear" class="form-control">
+											<option>2014</option>
+											<option>2015</option>
+											<option>2016</option>
+											<option>2017</option>
+											<option>2018</option>
+										</select> <!-- End select -->										
+									</div> <!-- End form-group -->
+								</div> <!-- End column -->
+								<div class="col-md-4">
+									<div class="form-group">
+										<label class="control-label" for="major">Major</label>
+										<select id="major" name="major" class="form-control">
+											<option>Accounting</option>
+											<option>Computer Science</option>
+											<option>Physics</option>
+											<option>Mathematics</option>
+											<option>Economics</option>
+										</select> <!-- End select -->										
+									</div> <!-- End form-group -->
+								</div> <!-- End column -->							
+							</div> <!-- End Row -->	
+							<div class="row">
+								<div class="col-md-4">
+									<div class="form-group">
+										<label class="control-label" for="GPA">GPA</label>
+										<input id="gpa" type="text" class="form-control" name="gpa" placeholder="GPA">
+									</div> <!-- End form-group -->
+								</div> <!-- End column -->								
+							</div> <!-- End row -->	
+							<div class="row">
+								<div class="col-md-8">
+									<label class="control-label" for="aboutMe">About Me</label>
+									<textarea id="aboutMe" class="form-control" name="aboutMe" placeholder="Fill this area with previous experience and relevant qualifications."></textarea>
+							</div> <!-- End row -->					
 						</form> <!-- End form -->					
 					</div> <!-- End modal-body -->
 				</div> <!-- End modal-content -->
@@ -185,77 +181,58 @@
 							<h3>Filter Constraints</h3>
 						</div> <!-- end row -->
 						<div class="row">
-							<form class="form-horizontal" id="findAccountForm" method="post" action="findStudentAccount.php">
+							<form class="form-horizontal" id="searchUsersForm" method="post" action="searchUsers.php">
 								<div class="row">
 									<div class="col-md-10">
 											<label class="control-label" for="emailSearch">Email</label>
-												<input id="emailSearch" type="email" class="form-control" name="emailSearch" placeholder="Email"/>																				
+											<input id="emailSearch" type="email" class="form-control" name="emailSearch" placeholder="Email">																				
 									</div> <!-- End column -->					
 								</div> <!-- End row -->
 								<div class="row">
 									<div class="col-md-10">
 											<label class="control-label" for="firstName">First Name</label>
-												<input id="firstName" type="text" class="form-control" name="firstName" placeholder="First Name"/>																				
+												<input id="firstName" type="text" class="form-control" name="firstName" placeholder="First Name">																				
 									</div> <!-- End column -->					
 								</div> <!-- End row -->
 								<div class="row">
 									<div class="col-md-10">
 											<label class="control-label" for="lastName">Last Name</label>
-												<input id="lastName" type="text" class="form-control" name="lastName" placeholder="Last Name"/>																				
+												<input id="lastName" type="text" class="form-control" name="lastName" placeholder="Last Name">																				
 									</div> <!-- End column -->					
 								</div> <!-- End row -->								
 								<br>
 								<div class="row">
 									<div class="col-md-6">
-										<button id="searchStudents"  type="submit" name="searchStudents" class="btn btn-success btn-block"><span class="glyphicon glyphicon-search"></span> Search</button>
+										<button id="searchButton"  type="submit" name="searchButton" class="btn btn-success btn-block"><span class="glyphicon glyphicon-search"></span> Search</button>
 									</div> <!-- End column -->
 								</div> <!-- End row -->	
 							</form> <!-- End form-horizontal -->
-						</div>
+						</div> <!-- End row -->
 					</div> <!-- End container -->	
-				<div class="container" id="results">
-					<div class="row">
-						<div class="col-md-4">
-							<h3>Results</h3>
-						</div> <!-- End column -->
-					</div> <!-- End container -->	
-					<div class="row">
-						<div class="col-md-12">
-							<table class="table table-striped">
-										<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>Course</th><th>Type</th> <th>GPA</th><th>Profile</th></tr>									
-										<tr><td>3</td> <td>Jinze</td> <td>Ahn</td> <td>jan2@u.rochester.edu</td><td>CSC171</td><td>Lab TA</td><td>4.00</td><td><a type="button" type="button" data-toggle="modal" href="#editProfileID" class="btn btn-default">
-											<span class="glyphicon glyphicon-wrench"></span> Profile</a>
-											</td>
-										</tr> 
-										<tr><td>3</td> <td>Jinze</td> <td>Ahn</td> <td>jan2@u.rochester.edu</td><td>CSC171</td><td>Lab TA</td><td>4.00</td><td><a type="button" type="button" data-toggle="modal" href="#editProfileID" class="btn btn-default">
-											<span class="glyphicon glyphicon-wrench"></span> Profile</a>
-											</td>
-										</tr> 	
-										<tr><td>3</td> <td>Jinze</td> <td>Ahn</td> <td>jan2@u.rochester.edu</td><td>CSC171</td><td>Lab TA</td><td>4.00</td><td><a type="button" type="button" data-toggle="modal" href="#editProfileID" class="btn btn-default">
-											<span class="glyphicon glyphicon-wrench"></span> Profile</a>
-											</td>
-										</tr> 											
-							</table> <!-- End table table-striped -->									
-						</div> <!-- End column -->
-					</div> <!-- End row -->
-					<div class="row">
-						<div class="col-md-4">
-						</div> <!-- End column -->
-						<div class="col-md-4">
-							<ul class="pagination navbar-right">
-								<li class="disabled"><span>&laquo;</span></li>
-								<li class="active"><a href="#"><span>1 <span class="sr-only">(current)</span></span></a></li>
-								<li ><a href="#"><span>2 <span class="sr-only">(current)</span></span></a></li>
-								<li ><a href="#"><span>3 <span class="sr-only">(current)</span></span></a></li>
-								<li ><a href="#"><span>4 <span class="sr-only">(current)</span></span></a></li>
-								<li class="disabled"><span>&laquo;</span></li>
-							</ul>							
-						</div> <!-- End column -->
-					</div> <!-- End row -->
-				</div> <!-- End results container -->	
-				
-				</div> <!-- End Outer-container -->
-			</div>
+					
+					<div class="container" id="result">
+						<div class="row">
+							<div class="col-md-4">
+								<h3 class="btn-default">Results</h3>
+							</div> <!-- End column -->
+						</div> <!-- End row -->						
+						<div class="row">
+							<div class="col-md-12">
+								<table class="table table-striped" id="resultTable">
+									<tr><th>First Name</th><th>Last Name</th><th>email</th><th>Profile</th><tr>
+								</table> <!-- End table table-striped -->									
+							</div> <!-- End column -->
+						</div> <!-- End row -->
+						<!-- TODO: Implement pagination every 10 rows -->				
+					</div> <!-- End container -->
+					<div class="jumbotron" id="emptyResult">
+						<div class="row" id="testRow" data-id="14">
+							<div class="col-md-12">
+								<p>No Students Found</p>
+							</div> <!-- End column -->
+						</div> <!-- End row -->
+					</div> <!-- End jumbotron no results -->					
+				</div>
 			<!-- END Page Content --> 
 	    
 			<!--BEGIN Page Footer -->
@@ -283,6 +260,5 @@
 			<!--END Page Footer -->
 		</div> 
 		<!-- End page-wrapper -->
-		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	</body>	
 </html>

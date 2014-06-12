7a4e9116a11a806c098f2bbcb914ebfb060e1cbb
raign <jan2@u.rochester.edu>
2014-06-12 17:52:51 -0400

    *SIGH* pushing in pairs.

--- student/withdraw.php
+++
@@ -1,9 +0,0 @@
-<?php
-	include('../db.php');
-	if(!empty($_POST)){
-		$positionID = $_POST['positionID'];
-		$studentID = $_POST['studentID'];
-		$student = User::getUserByID($studentID, STUDENT);
-		$student->withdraw($positionID);
-	}
-?>

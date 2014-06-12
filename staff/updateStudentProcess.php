bee01b55c1f0c8ded738c37b55631b301706492c
Oscar Rojas <orojas@u.rochester.edu>
2014-06-12 14:52:16 -0400

    fixed bug with class year and gpa

--- staff/updateStudentProcess.php
+++ staff/updateStudentProcess.php
@@ -1,5 +1,5 @@
 <?php
 	include('../db.php');
 	$student = User::getUserByEmail($_POST['email'],STUDENT);
-	$student->updateProfile($_POST['firstName'],$_POST['lastName'],$_POST['mobilePhone'],$_POST['classYear'],$_POST['major'],$_POST['gpa'],$_POST['aboutMe']);	
+	$student->updateProfile($_POST['firstName'],$_POST['lastName'],$_POST['mobilePhone'],$_POST['major'],$_POST['classYear'],$_POST['gpa'],$_POST['aboutMe']);	
 ?>

aab4f8d1332527010105bbaa1b51800378fe5a8a
Oscar Rojas <orojas@u.rochester.edu>
2014-06-11 23:29:31 -0400

    Fixed script to fetch results and return a JSON object.

---
+++ staff/searchUsers.php
@@ -0,0 +1,16 @@
+<?php
+	ini_set("display_errors",1);
+	include('../db.php');
+	$usersFound = User::findUsers($_POST['emailSearch'],$_POST['firstName'],$_POST['lastName'],STUDENT);
+	$usersTable = array();
+	
+	/* Add each user to the usersTable to be encoded into a JSON object */
+	foreach($usersFound as $user){
+		$firstName = $user->getFirstName();
+		$lastName = $user->getLastname();
+		$email = $user->getEmail();
+		$userID = getUserID($email);
+		$usersTable[] = array('firstName' => $firstName,'lastName' => $lastName,'email' => $email,'userID' => $userID);
+	}
+	echo json_encode($usersTable);
+?>

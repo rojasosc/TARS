28af6939807732a0f488293b76a3043c087ee7d9
Oscar Rojas <orojas@u.rochester.edu>
2014-06-13 11:24:18 -0400

    fixed undefined call to homePhone

--- process.php
+++ process.php
@@ -1,6 +1,6 @@
 <?php
 	include ("db.php");
 	
-	registerStudent($_POST['firstName'],$_POST['lastName'],$_POST['email'],$_POST['password'],$_POST['homePhone'],$_POST['mobilePhone'],$_POST['classYear'],$_POST['major'],$_POST['gpa'],$_POST['aboutMe'],$_POST['universityID']);
+	registerStudent($_POST['firstName'],$_POST['lastName'],$_POST['email'],$_POST['password'],$_POST['mobilePhone'],$_POST['classYear'],$_POST['major'],$_POST['gpa'],$_POST['aboutMe'],$_POST['universityID']);
 
 ?>

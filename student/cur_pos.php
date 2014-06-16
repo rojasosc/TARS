7296347968d4a31cdebed4c59b17e57415a6c5e2
raign <jan2@u.rochester.edu>
2014-06-16 12:37:31 -0400

    cur pos update
    bug still not fixed though..

--- student/cur_pos.php
+++ student/cur_pos.php
@@ -27,30 +27,30 @@
 	</head>
   
 	<body>
-		<!-- BEGIN Withdraw Modal -->
-		<div class="modal fade" id="withdrawModal" tabindex="-1" role="dialog" aria-labelledby="withdrawModalLabel" aria-hidden="true">
+		<!-- BEGIN Release Modal -->
+		<div class="modal fade" id="releaseModal" tabindex="-1" role="dialog" aria-labelledby="releaseModalLabel" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h1 class="modal-title">Withdraw From Position</h1>
+						<h1 class="modal-title">Release From Position</h1>
 					</div>
 					<div class="modal-body">
-						<form action="withdraw.php" method="post" class="withdrawForm">
+						<form action="withdraw.php" method="post" id="releaseForm">
 							<fieldset>
 								<div class="row">
 									<div class="col-xs-10 col-xs-offset-1">
 										<p>
-											Are you sure you want to withdraw from this position? It is highly unlikely for you to get it back after you withdraw from it. You will no longer be responsible for this position but you will also relinquish all remaining compensations for filling this position.
+											Are you sure you want to be released from this position? It is highly unlikely for you to get it back after you're released from it. You will no longer be responsible for this position but you will also relinquish all remaining compensations for filling this position.
 										</p>
 									</div>
 								</div>
 								<div class="row">
 									<div class="col-xs-10 col-xs-offset-1">
 										<p>
-											If you still wish to withdraw from this position, an E-mail will be sent to your employer notifying them of your release and your reasons detailed below:
+											If you still wish to be released from this position, an E-mail will be sent to your employer notifying them of your release and your reasons detailed below:
 										</p>
-										<textarea class="form-control" rows="8" cols="64" form="withdrawReason">
+										<textarea class="form-control" rows="8" cols="64" form="releaseForm" name="releaseReasons">
 										</textarea>
 									</div>
 								</div>
@@ -59,13 +59,43 @@
 						</form>
 					</div>
 					<div class="modal-footer">
-						<button type="button" class="btn btn-danger" data-dismiss="modal" id="cancelWithdraw">Cancel</button>
+						<button type="button" class="btn btn-danger" data-dismiss="modal" id="cancelRelease">Cancel</button>
+						<button type="submit" class="btn btn-success" form="releaseForm" id="#releaseConfirm" value="Submit">Release</button>
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- END Release Modal -->
+		<!-- BEGIN Withdraw Modal -->
+		<div class="modal fade" id="withdrawModal" tabindex="-1" role="dialog" aria-labelledby="withdrawModalLabel" aria-hidden="true">
+			<div class="modal-dialog modal-sm">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h1 class="modal-title">Withdraw Application</h1>
+					</div>
+					<div class="modal-body">
+						<form action="withdraw.php" method="post" id="withdrawForm">
+							<fieldset>
+								<div class="row">
+									<div class="col-xs-10 col-xs-offset-1">
+										<p>
+											Are you sure you want to withdraw your application?
+										</p>
+									</div>
+								</div>
+								<input id="studentID" type="hidden" value="<?=$student->getID()?>" />
+							</fieldset>
+						</form>
+					</div>
+					<div class="modal-footer">
+						<button type="button" class="btn btn-danger" data-dismiss="modal" id="withdrawCancel">Cancel</button>
 						<button type="submit" class="btn btn-success" form="withdrawForm" id="#withdrawConfirm" value="Submit">Withdraw</button>
 					</div>
 				</div>
 			</div>
 		</div>
-		<!-- END Email Modal -->
+		<!-- END Release Modal -->
 		<!-- BEGIN page-wrapper -->
             
 		<div id="page-wrapper">
@@ -134,7 +164,7 @@
 								<td><?= "TBD"?></td>
 								<td><?= $position->getTime()?></td>
 								<td><?= $row->getCompensation()?></td>
-								<td><a class="btn btn-default withdrawButton" href="#withdrawModal" data-toggle="modal"><span class="glyphicon glyphicon-remove"></span></a></td>
+								<td><a class="btn btn-default releaseButton" href="#releaseModal" data-toggle="modal"><span class="glyphicon glyphicon-remove"></span></a></td>
 							</tr>
 							<?php
 		}

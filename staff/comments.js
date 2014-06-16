e1e4a9ad1e3e17fc070953e2d5ef41ad7bfeee0e
Oscar Rojas <orojas@u.rochester.edu>
2014-06-16 17:35:53 -0400

    New js script used to handle staff comments

---
+++ staff/comments.js
@@ -0,0 +1,36 @@
+$(document).ready(function () {
+	$commentModal = $('#commentModal');
+	$commentForm = $('#commentForm');
+	var studentID = null;
+	var commenterID = null;
+	
+	$commentForm.submit(function () { return false; });
+	$('.comment').click(prepareCommentModal);
+	$('#submitCommentButton').click(submitComment);
+	$commentModal.bind('hidden.bs.modal',clearCommentForm);
+});
+
+function submitComment(){
+	var url = $commentForm.attr('action');
+	var data = {
+		/*TODO: Support a subject line. */
+		studentID: studentID,
+		commenterID: commenterID,
+		comment: $("[name='commentText']").val()
+	}
+	studentID = null;
+	commenterID = null;
+	$.post(url,data,function () { });
+	clearCommentForm();
+	$commentModal.modal('hide');
+}
+
+function prepareCommentModal(){
+	studentID = $(this).data('studentid');
+	commenterID = $(this).data('staffid');
+	$commentModal.modal('show');
+}
+
+function clearCommentForm(){
+	$commentForm.trigger("reset");
+}

acddb705c027f592644ce875b577ffde2c97d865
Nate Book <nmbook@gmail.com>
2014-06-17 14:38:18 -0400

    Catch exceptions in emailExists.php

--- emailExists.php
+++ emailExists.php
@@ -4,7 +4,14 @@ include('db.php');
 $result = array('valid' => false);
 
 if (isset($_POST['email'])) {
-	$result['valid'] = User::checkEmailAvailable($_POST['email']);
+	try {
+		$result['valid'] = User::checkEmailAvailable($_POST['email']);
+	} catch (PDOException $ex) {
+		// valid = false on error
+		Error::setError(Error::EXCEPTION, 'Error checking e-mail availability.',
+			$ex);
+		$result['error'] = Error::getError()->toArray();
+	}
 }
 
 echo json_encode($result);

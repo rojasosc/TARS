a853e73f636a54dd57d04506fc2c5b6f0c2dd537
Nate Book <nmbook@gmail.com>
2014-06-17 14:42:38 -0400

    Catch exceptions in professor/fetchStudent.php and always return JSON.

--- professor/fetchStudent.php
+++ professor/fetchStudent.php
@@ -1,23 +1,23 @@
 <?php
-	include('../db.php');
-	$user = User::getUserByID($_POST['userID']);
-	$student = array();
-	if($user){
-		$firstName = $user->getFirstName();
-		$lastName = $user->getLastName();
-		$email = $user->getEmail();
-		$mobilePhone = $user->getMobilePhone();
-		$classYear = $user->getClassYear();
-		$major = $user->getMajor();
-		$gpa = $user->getGPA();
-		$aboutMe = $user->getAboutMe();
-		
-		/* Prepare to encode JSON object */
-		$student = array('firstName' => $firstName,'lastName' => $lastName,'email' => $email,'mobilePhone' => $mobilePhone,
-		'classYear' => $classYear,'major' => $major,'gpa' => $gpa,'aboutMe' => $aboutMe);
-		echo json_encode($student,true);
-	}else{
-		echo json_encode(false);
+include('../db.php');
+$student = array();
+if (isset($_POST['userID'])) {
+	try {
+		$user = User::getUserByID($_POST['userID']);
+		if ($user) {
+			$student['firstName'] = $user->getFirstName();
+			$student['lastName'] = $user->getLastName();
+			$student['email'] = $user->getEmail();
+			$student['mobilePhone'] = $user->getMobilePhone();
+			$student['classYear'] = $user->getClassYear();
+			$student['major'] = $user->getMajor();
+			$student['gpa'] = $user->getGPA();
+			$student['aboutMe'] = $user->getAboutMe();
+		}
+	} catch (PDOException $ex) {
+		Error::setError(Error::EXCEPTION, 'Error getting user object.', $ex);
+		$student['error'] = Error::getError()->toArray();
 	}
-	
-?>
+}
+echo json_encode($student, true);
+

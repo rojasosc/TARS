4e436c120d8f30ee1bece13a81fc7e9ce6ecd61e
Oscar Rojas <orojas@u.rochester.edu>
2014-06-27 14:07:37 -0400

    Deleted unnecessary php files

--- staff/newComment.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include('../db.php');
-	ini_set('display_errors',1);
-	Feedback::newComment($_POST['studentID'],$_POST['commenterID'],$_POST['comment']);
-?>
--- staff/reviewProcess.php
+++
@@ -1,5 +0,0 @@
-<?php
-	include('../db.php');
-	ini_set('display_errors',1);
-	Student::setStudentStatus($_POST['userID'],$_POST['status']);
-?>

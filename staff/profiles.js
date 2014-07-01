ff7b3934c5a782ac4dfd01a687bdfce5f387ba08
Oscar Rojas <orojas@u.rochester.edu>
2014-07-01 17:02:54 -0400

    Added staff and professor comments to the student modals

--- staff/profiles.js
+++ staff/profiles.js
@@ -1,5 +1,8 @@
 $(document).ready(function () {	
 	$('.profile').click(displayProfile);
+	$staffComments = $('.staffComments');
+	$professorComments = $('.professorComments');
+
 });
 
 function displayProfile(){
@@ -16,6 +19,8 @@ function displayProfile(){
 }
 
 function fillProfile(user){
+	$staffComments.html('');
+	$professorComments.html('');
 	/* Associative array of a user */
 	var student = eval('(' + user + ')');
 	$('#studentModalTitle').html(student['firstName']+" "+student['lastName']);
@@ -25,4 +30,13 @@ function fillProfile(user){
 	$('#studentMobilePhone').html("Mobile Phone: " + student['mobilePhone']);
 	$('#studentAboutMe').html(student['aboutMe']);
 	$('#studentClassYear').html("Class Year: " + student['classYear']);
+	for (var i = student['staffComments'].length - 1; i >= 0; i--) {
+		var comment = student['staffComments'][i];
+		$staffComments.append('<div class="row"><div class="col-xs-4"><p>Date: ' + comment['dateTime'] + '</p></div><!-- End column --></div><!-- End row --><div class="row"><div class="col-xs-4"><p>Written By: ' + comment['firstName']+ ' ' + comment['lastName'] + '</p></div> <!-- End column --></div> <!-- End row --><div class="row"><div class="col-xs-12"><p>Message: ' + comment['comment'] + '</p></div><!-- End column --></div> <!-- End row --><br>');
+		
+	};
+	for (var i = student['professorComments'].length - 1; i >= 0; i--) {
+		var comment = student['professorComments'][i];
+		$professorComments.append('<div class="row"><div class="col-xs-4"><p>Date: ' + comment['dateTime'] + '</p></div><!-- End column --></div><!-- End row --><div class="row"><div class="col-xs-4"><p>Written By: ' + comment['firstName']+ ' ' + comment['lastName'] + '</p></div> <!-- End column --></div> <!-- End row --><div class="row"><div class="col-xs-12"><p>Message: ' + comment['comment'] + '</p></div><!-- End column --></div> <!-- End row --><br>');
+	};
 }

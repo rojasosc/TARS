51b2dce39c3b576f8e1b5c8173f6f2b11cd32f74
Oscar Rojas <orojas@u.rochester.edu>
2014-07-12 19:16:33 -0400

    New applicants UI design. Each course is represented using a panel with each section nested inside the panel.

--- professor/applicants.php
+++ professor/applicants.php
@@ -1,4 +1,5 @@
 <?php
+//ini_set("display_errors",1);
 require_once 'professorSession.php';	
 
 $term = null;
@@ -14,6 +15,7 @@ if ($error == null) {
 		$error = new TarsException(Event::SERVER_DBERROR, Event::USER_GET_SECTIONS, $ex);
 	}
 }
+$courses = $professor->getCourses();
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -99,170 +101,211 @@ if ($error == null || $error->getAction() != Event::SESSION_CONTINUE) {
 				<div class="row">
 					<h1 class="panelHeader">My Applicants</h1>
 				</div> <!-- End row -->			
-							
+				<div class="container">
 				<?php
-				/*Obtain positionIDS that are in the Assistantship table
-				and that match a particular CRN	
-				Pack these applications into a panel... repeat.
-				 */
-				$tableEntry = 0;
-				foreach($sections as $section){
-					$applications = array();
-					try {
-						/* applications for this particular section */
-						$applications = Application::getApplications($section, $professor, $term, PENDING);
-					} catch (PDOException $ex) {
-						$error = new TarsException(Event::SERVER_DBERROR,
-							Event::USER_GET_APPLICATIONS, $ex);
-						echo $error->toHTML();
-					}
-
-					/* create a new panel */ 
-					$panelID = "panelID" . $section->getID();
-
-					/*TODO: Get total positions of a particular type and course.
-					For instance, all the graders for CSC 172.*/
-
-					$positionTypes = Position::getAllPositionTypes(true);
-					$positionTotals = array();
-					foreach ($positionTypes as $typeID => $title) {
-						$data = array(
-							'typeID' => $typeID,
-							'title' => $title,
-							'total' => $section->getTotalPositionsByType(
-								$professor, $typeID),
-							'current' => $section->getCurrentPositionsByType(
-								$professor, $typeID));
-						if ($data['total'] > 0) {
-							$data['ratio'] = $data['current'] / $data['total']; 
-							if ($data['ratio'] == 1) {
-								$data['alert'] = 'success';
-							} elseif ($data['ratio'] == 0) {
-								$data['alert'] = 'danger';
-							} else {
-								$data['alert'] = 'warning';
-							}
-							$positionTotals[] = $data;
-						}
-					}
-
-					$appCount = count($applications);
-					if ($appCount == 0) {
-						$appCount = '';
-					}
-					
-					/*TODO: Mark workshop super leader positions in the db so that we 
-					 can highlight them on professor pages. */
-					 
-					 /* Determine the color of the progress bars based on current/total ratio */
-				?>
-				<div class="row">
-					<div class="container">
+					foreach($courses as $course) {
+						$sections = $professor->getSectionsByCourseID($course['courseID']);
+						$applications = Application::getApplicationsByCourseID($course['courseID'],$professor);
+						$panelHeading = $course['department'] . " " . $course['courseNumber'] . " " . $course['courseTitle'];
+						$panelID = "course".$course['courseNumber'];
+						?>
 						<div class="panel panel-primary">
-							<div class="panel-heading" data-toggle="collapse" data-target="#<?=$panelID?>">
-							<h4 class="panel-title panelHeader"><?= $section->getCourseName() ?> <small><?=$section->getCourseTitle()?></small><span class="badge alert-warning pull-left"><?=$appCount?></span><span class="glyphicon glyphicon-chevron-right pull-right"></span></h4>
+							<div class="panel-heading" data-toggle="collapse" data-target="#<?= $panelID ?>">
+								<h4 class="panel-title panelHeader"><?= $panelHeading ?> <span class="glyphicon glyphicon-chevron-right pull-right"></span></h4>
 							</div> <!-- End panel-heading -->
 							<div class="collapse panel-collapse" id="<?= $panelID ?>">
 								<div class="panel-body">
-									<table class="table table-striped">
-										<thead>
-											<tr>
-												<th>Name</th>
-												<th>University ID</th>
-												<th>Type</th>
-												<th>Application</th>
-												<th>Reviews</th>
-											</tr>
-										</thead>
-										<?php
-										
-										/* Insert each application */
-										foreach($applications as $application){
-											$student = $application->getCreator();
-											$position = $application->getPosition();
-											$profileID = "myProfile" . $student->getID();
-											
-										?>
-										<tr>
-											<td>
-												<div class="dropdown actions">
-													<a class="dropdown-toggle" type="button" id="actionsMenu" data-toggle="dropdown">
-													<?= $student->getFirstName() . " " . $student->getLastName()?>
-													<span class="caret"></span>
-													</a>
-													<ul class="dropdown-menu" role="menu" id="actionsMenu" aria-labelledby="actionsMenu" data-applicationID="<?= $application->getID() ?>">
-														<li class="decision" role="presentation" data-decision="<?= APPROVED ?>"><a role="menuitem" tabindex="-1">Approve</a></li>
-														<li class="decision" role="presentation" data-decision="<?= REJECTED ?>"><a role="menuitem" tabindex="-1">Reject</a></li>
-														<li class="decision" role="presentation" data-decision="<?= PENDING ?>"><a role="menuitem" tabindex="-1">Postpone</a></li>
-													</ul>
-												</div>
-											</td>
-											<td>
-												<?= $student->getUniversityID() ?>
-											</td> 
-											<td>
-												<?= $position->getTypeTitle() ?>
-											</td>
-											<td>
-												<button data-toggle="modal" data-target="#profile-modal" data-appID="<?= $application->getID() ?>" data-usertype="<?= STUDENT ?>" data-userid="<?= $student->getID() ?>" class="btn btn-info circle profile">
-													<span class="glyphicon glyphicon-file"></span>
-												</button>
-											</td>
-											<td>
-												<button data-toggle="modal" data-target="#commentsModal" data-userID="<?= $student->getID() ?>" class="btn btn-info comments">
-													<span class="glyphicon glyphicon-comment"></span>
-												</button>
-											</td>
-										</tr> 												
-											
-											<?php
-											/* Table entry closing brace */
-											$tableEntry++;
+									
+						<?php
+							foreach($sections as $section){
+								$sessions = $section->getAllSessions();
+								$applications = array();
+								try {
+									/* applications for this particular section */
+									$applications = Application::getApplications($section, $professor, $term, PENDING);
+								} catch (PDOException $ex) {
+									$error = new TarsException(Event::SERVER_DBERROR,
+										Event::USER_GET_APPLICATIONS, $ex);
+									echo $error->toHTML();
+								}
+
+								/* create a new panel */ 
+								$panelID = "panelID" . $section->getID();
+
+								/*TODO: Get total positions of a particular type and course.
+								For instance, all the graders for CSC 172.*/
+
+								$positionTypes = Position::getAllPositionTypes(true);
+								$positionTotals = array();
+								foreach ($positionTypes as $typeID => $title) {
+									$data = array(
+										'typeID' => $typeID,
+										'title' => $title,
+										'total' => $section->getTotalPositionsByType(
+											$professor, $typeID),
+										'current' => $section->getCurrentPositionsByType(
+											$professor, $typeID));
+									if ($data['total'] > 0) {
+										$data['ratio'] = $data['current'] / $data['total']; 
+										if ($data['ratio'] == 1) {
+											$data['alert'] = 'success';
+										} elseif ($data['ratio'] == 0) {
+											$data['alert'] = 'danger';
+										} else {
+											$data['alert'] = 'warning';
+										}
+										$positionTotals[] = $data;
+									}
+								}
+
+								$appCount = count($applications);
+								if ($appCount == 0) {
+									$appCount = '';
+								} ?>
+											<div class="row">
+												<!-- Section description -->
+												<div class="col-xs-2">
+													<legend><?= ucfirst($section->getSectionType()) ?>
+														<small>
+															<?php
+																foreach($sessions as $session){
+																	$days = $session->getWeekDays();
+																	$time = $session->getStartTime() . "-" . $session->getEndTime();
+																	$building = $session->getPlaceBuilding();
+																	$room = $session->getPlaceRoom();
+																	$location = $building . " " . $room;
+																?>
+																	<p><?= $days . " " . $time ?></p>
+
+															<?php
+
+																}
+															?>
+															<p><?= $location ?></p>  
+														</small>
+													</legend>													
+												</div> <!-- End column -->
+												<!-- Applications -->
+												<div class="col-xs-8">
+														<?php
+															if(!$applications){
+														?>
+															<div class="alert alert-danger" role="alert">
+																<p>There are currently no available applications for this section.</p>
+															</div>
+															
+														<?php
+															}else{																
+
+														?>
+
+												<table class="table table-striped">
+													<thead>
+														<tr>
+															<th>Name</th>
+															<th>University ID</th>
+															<th>Type</th>
+															<th>Application</th>
+															<th>Reviews</th>
+														</tr>
+													</thead>
+													<?php
+
+													/* Insert each application */
+													foreach($applications as $application){
+														$student = $application->getCreator();
+														$position = $application->getPosition();
+														$profileID = "myProfile" . $student->getID();
+
+													?>
+													<tr>
+														<td>
+															<div class="dropdown actions">
+																<a class="dropdown-toggle" type="button" id="actionsMenu" data-toggle="dropdown">
+																<?= $student->getFirstName() . " " . $student->getLastName()?>
+																<span class="caret"></span>
+																</a>
+																<ul class="dropdown-menu" role="menu" id="actionsMenu" aria-labelledby="actionsMenu" data-applicationID="<?= $application->getID() ?>">
+																	<li class="decision" role="presentation" data-decision="<?= APPROVED ?>"><a role="menuitem" tabindex="-1">Approve</a></li>
+																	<li class="decision" role="presentation" data-decision="<?= REJECTED ?>"><a role="menuitem" tabindex="-1">Reject</a></li>
+																	<li class="decision" role="presentation" data-decision="<?= PENDING ?>"><a role="menuitem" tabindex="-1">Postpone</a></li>
+																</ul>
+															</div>
+														</td>
+														<td>
+															<?= $student->getUniversityID() ?>
+														</td> 
+														<td>
+															<?= $position->getTypeTitle() ?>
+														</td>
+														<td>
+															<button data-toggle="modal" data-target="#profile-modal" data-appID="<?= $application->getID() ?>" data-usertype="<?= STUDENT ?>" data-userid="<?= $student->getID() ?>" class="btn btn-info circle profile">
+																<span class="glyphicon glyphicon-file"></span>
+															</button>
+														</td>
+														<td>
+															<button data-toggle="modal" data-target="#commentsModal" data-userID="<?= $student->getID() ?>" class="btn btn-info comments">
+																<span class="glyphicon glyphicon-comment"></span>
+															</button>
+														</td>
+													</tr> 												
+														
+														<?php
+														/* Table entry closing brace */
+														$tableEntry++;
+														}
+
+														?>
+												</table> <!-- End table -->														
+														<?php
+
+															}
+														?>
+
+												</div> <!-- End column -->
+												<!-- Progress --> 
+												<div class="col-xs-2">
+													<div class="panel-footer">
+														<div class="row">
+						<?php
+											$totalsColumnWidth = 12;
+											while ($totalsColumnWidth > 0 &&
+												count($positionTotals) * $totalsColumnWidth > 12) {
+												$totalsColumnWidth--;
 											}
-											
-											?>
-									</table> <!-- End table -->
-								</div> <!-- End panel-body -->									
-							</div> <!-- End collapse panel-collapse -->
+											foreach ($positionTotals as $totalBar) {
+						?>
+															<div class="col-xs-<?=$totalsColumnWidth?>">
+																<strong><?=$totalBar['title']?>s</strong>
+																<div class="progress progress-striped active">
+																	<div class="progress-bar progress-bar-<?= $totalBar['alert'] ?>"  role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: <?= $totalBar['ratio'] * 100 ?>%">
+																	</div> <!-- End progress-bar progress-bar-danger -->
+																</div> <!-- End progress-bar -->												
+															</div> <!-- End column -->
+						<?php
+											}
+						?>
+														</div> <!-- End row -->
+													</div> <!-- End panel-footer -->																								
+												</div> <!-- End column -->
+											</div> <!-- End row -->
+											<hr>
+						<?php
+							}
+						?>
+									
+								</div> <!-- End panel-body -->
+							</div> <!-- End panel panel-collapse -->
 							<div class="panel-footer">
-								<div class="row">
-<?php
-					$totalsColumnWidth = 12;
-					while ($totalsColumnWidth > 0 &&
-						count($positionTotals) * $totalsColumnWidth > 12) {
-						$totalsColumnWidth--;
-					}
-					foreach ($positionTotals as $totalBar) {
-?>
-									<div class="col-xs-<?=$totalsColumnWidth?>">
-										<strong><?=$totalBar['title']?>s</strong>
-										<div class="progress progress-striped active">
-											<div class="progress-bar progress-bar-<?= $totalBar['alert'] ?>"  role="progressbar" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100" style="width: <?= $totalBar['ratio'] * 100 ?>%">
-											</div> <!-- End progress-bar progress-bar-danger -->
-										</div> <!-- End progress-bar -->												
-									</div> <!-- End column -->
-<?php
-					}
-?>
-								</div> <!-- End row -->
 							</div> <!-- End panel-footer -->
 						</div> <!-- End panel panel-primary -->
-					</div> <!-- End container -->
-				</div> <!-- End row -->
-				<?php
-				/* Course panels closing brace */
-				}
-				if (count($sections) == 0) {
-?>
-<div class="alert alert-info">There are no sections assigned to you this term.</div>
-<?php
-				}				
-				?>
 
-				<!-- END Course Panels -->
-<?php
-}
-?>
+				<?php	
+
+					}
+				}
+				?>
+			</div> <!-- End panel container -->
 			</div>
 			<!-- END Page Content --> 
 	    

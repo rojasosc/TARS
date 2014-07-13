1f592e492ca8478c7bfa1293221f4b23073a3ba7
Oscar Rojas <orojas@u.rochester.edu>
2014-07-13 19:24:50 -0400

    Updated page layout with new announcements and notifications column.

--- professor/professor.php
+++ professor/professor.php
@@ -15,7 +15,7 @@ if ($error != null) {
 		$error = new TarsException(Event::SERVER_DBERROR, Event::USER_GET_APPLICATIONS, $ex);
 	}
 }
-
+$unfilledPositions = false; /*TODO: Check for the existence of vacant positions */
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -44,20 +44,74 @@ if ($error != null) {
 }
 if ($error == null || $error->getAction() != Event::SESSION_CONTINUE) {
 ?>
-			    <div class="row">
-					<div class="container">
-						<div class="jumbotron">
-							<h2>Welcome Professor <?= $professor->getLastName() ?>!</h2>
-							
-							<h3>Notifications</h3> 
-							<p> You have <?= $pendingApps ?> <a href="applicants.php" >applications</a> pending!</p> 
-							
-							<h3>Announcements</h3>
-							<p>Remember to submit feedback for your assistants by (date).</p>
-							<p>Your feedback helps rank assistants by their past experience.</p>
-						</div> <!-- End jumbotron -->
-					</div> <!-- End container -->
-				</div> <!--End Row -->
+				<div class="container">
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h4 class="panel-title panelHeader">Home</h4>
+						</div> <!-- End panel-heading -->
+						<div class="panel-body">
+								<div class="row">
+									<div class="col-xs-6">
+										<div class="row">
+											<div class="col-xs-12">
+												<h3>Announcements</h3>
+											</div> <!-- End column -->
+										</div> <!-- End row --> 								
+									    <div class="row">
+									    	<div class="col-xs-12">
+													<p>Remember to submit reviews for your <a href="assistants.php">assistants</a> to help other
+														professors make informed decisions.</p>							    			
+									    	</div> <!-- End column -->
+										</div> <!--End Row -->		
+									</div> <!-- End column -->
+									<div class="col-xs-6">
+										<div class="row">
+											<div class="col-xs-12">
+												<h3>Notifications</h3>
+											</div> <!-- End column -->
+										</div> <!-- End row -->
+										<?php
+											if(!$professor->getSections()){
+										?>
+										<div class="row">
+											<div class="col-xs-12">
+													<div class="alert alert-danger" role="alert">
+															You have not yet been assigned any sections. 
+													</div> <!-- End alert alert-danger -->
+											</div> <!-- End column -->									
+										</div> <!-- End row -->
+										<?php		
+											}
+											if($professor->getSections() && $unfilledPositions){
+										?>
+										<div class="row">
+											<div class="col-xs-12">
+													<div class="alert alert-danger" role="alert">
+														There are still unfilled positions for some of your sections.<br>
+														Review <a href="applicants.php">pending applications</a> to fill these positions.
+													</div> <!-- End alert alert-danger -->
+											</div> <!-- End column -->									
+										</div> <!-- End row -->
+										<?php		
+											}
+											if($professor->getSections() && !$unfilledPositions){
+										?>
+										<div class="row">
+											<div class="col-xs-12">
+													<div class="alert alert-success" role="alert">
+															There are currently no notifications to display.
+													</div> <!-- End alert alert-danger -->
+											</div> <!-- End column -->									
+										</div> <!-- End row -->		
+										<?php		
+											}
+										?>									
+										</div> <!-- End row -->											
+									</div> <!-- End column -->									
+								</div> <!-- End row -->																			
+						</div> <!-- End panel-body -->
+					</div> <!-- End panel panel-primary -->
+				</div> <!-- End container -->
 <?php
 }
 ?>

61570fe657fa026433c91d8c625322536f856c2a
Oscar Rojas <orojas@u.rochester.edu>
2014-07-13 20:39:33 -0400

    updated page to display applications instead of students

--- staff/reviewStudents.php
+++ staff/reviewStudents.php
@@ -15,34 +15,32 @@ require_once 'staffSession.php';
 		<link href="staff.css" rel="stylesheet">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 		<script src="../js/bootstrap.min.js"></script>
-		<script src="reviewStudents.js"></script>
 		<script src="comments.js"></script>
 		<script src="../js/tars_utilities.js"></script>
 	</head>
 	<body>
-			
 		<!-- Profile Modal -->
 		<div class="modal fade profile-modal" id="profile-modal" tabindex="-1" role="dialog" aria-labelledby="studentProfileModal" aria-hidden="true">
 			<div class="modal-dialog">
 				<div class="modal-content">
 					<div class="modal-header">
 						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-						<h4 class="modal-title" id="studentModalTitle"></h4>
+						<h2 class="modal-title" id="studentModalTitle"></h2>
 					</div>
-					<div class="modal-body">			
-						<h3>Personal Information</h3>
+					<div class="modal-body">
+						<h3>Qualifications</h3>
+						<div class="container qualifications">
+						</div> <!-- End container -->									
+						<h3>Academic Information</h3>
 						<div class="container">
 							<p id="studentMajor"></p>
 							<p id="studentGPA"></p>
 							<p id="studentClassYear"></p>
-						</div> <!-- End container -->			
+						</div> <!-- End container -->								
 						<h3>Contact Information</h3>
 						<div class="container">
 							<p id="studentEmail"></p>
 							<p id="studentMobilePhone"></p>
-						</div> <!-- End container -->
-						<h3>Comments</h3>
-						<div class="container" id="comments">
 						</div> <!-- End container -->
 					</div> <!-- End modal body -->
 					<div class="modal-footer">
@@ -52,7 +50,61 @@ require_once 'staffSession.php';
 			</div> <!-- End modal-dialog -->
 		</div> <!-- End modal fade -->
 		<!-- End Profile Modal -->
+
+		 <!-- BEGIN Comment Modal-->
+		<div class="modal fade comments-modal" id="commentsModal" tabindex="-1" role="dialog" aria-labelledby="commentModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h1 class="modal-title">Reviews</h1>
+					</div> <!-- End modal-header -->
+					<div class="modal-body comments-block">
+					</div> <!-- End modal-body -->
+					<div class="modal-footer">
+						<button class="btn btn-danger" data-dismiss="modal">Close</button>
+					</div> <!-- End modal-footer -->				
+				</div> <!-- End modal-content -->
+			</div> <!-- End modal-dialog -->
+		</div> <!-- End modal fade -->	
+		<!-- END Comment Modal-->
 			
+			<!-- Begin Email Modal -->
+		<div class="modal fade" id="emailModal" tabindex="-1" role="dialog" aria-labelledby="emailModalLabel" aria-hidden="true">
+			<div class="modal-dialog">
+				<div class="modal-content">
+					<div class="modal-header">
+						<button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+						<h4 class="modal-title" id="myProfileLabel">TARS</h4>
+					</div> <!-- End modal-header -->
+					<div class="modal-body">	
+						<h3>Email Form</h3>
+						<div class="container">			
+							<form action="#" method="post">			
+								<fieldset>				
+									<div class="row">					
+										<div class="col-sm-8 col-md-6">
+											<label>Subject</label>
+											<input type="text" name="subjectLine" class="form-control" placeholder="Enter A Subject Line.">
+										</div> <!-- End column -->
+									</div><!-- End row -->					
+									<div class="row">					
+										<div class="col-sm-8 col-md-6">
+											<label>Message</label>
+											<textarea class="form-control" rows="3" placeholder="Enter Your Message."></textarea> <!-- End text area -->
+										</div> <!-- End column -->
+									</div> <!-- End row -->					
+								</fieldset>				
+							</form> <!-- End form -->
+						</div> <!-- End container -->
+					<div class="modal-footer">
+						<button type="button" data-dismiss="modal" class="btn btn-danger">Close</button> <!-- close modal -->
+					</div>	<!-- End modal footer -->
+					</div> <!-- End modal body -->
+				</div> <!-- End modal-content -->
+			</div> <!--End modal-dialog-->
+		</div> <!-- End modal fade -->  
+		 <!-- End Email Modal -->			
 		 <!-- BEGIN Comment Modal-->
 		<div class="modal fade" id="commentModal" tabindex="-1" role="dialog" aria-labelledby="commentModalLabel" aria-hidden="true">
 			<div class="modal-dialog">
@@ -64,7 +116,7 @@ require_once 'staffSession.php';
 						<small> For example, describe this assistant's productivity, enthusiasm, punctuality, initiative, or dependability.</small>
 					</div> <!-- End modal-header -->
 					<div class="modal-body">
-						<form action="professorCommands.php" method="post" id="commentForm" class="form-horizontal">
+						<form action="../actions.php" method="post" id="commentForm" class="form-horizontal">
 							<fieldset>
 								<div class="row">
 									<div class="col-xs-12">
@@ -99,58 +151,89 @@ if ($error != null) {
 }
 if ($error == null || $error->getAction() != Event::SESSION_CONTINUE) {
 ?>
-				<!-- Course Panels -->	
-				<div class="row">
-					<div class="container">					
-						<div class="panel panel-primary">
-							<div class="panel-heading">
-								<p class="panelHeader">Students</p>
-							</div> <!-- End panel-heading -->
-							<div class="panel-body">
-								<table class="table table-striped table-hover">
-									<tr><th>ID</th><th>First Name</th><th>Last Name</th><th>Email</th><th>GPA</th><th>Profile</th><th>Comment</th></tr>
-									<?php
-									
-									$studentsToReview = Student::getStudentsToReview();
-									$tableEntry = 0;
-									if($studentsToReview != NULL) {
-										foreach($studentsToReview as $student){
+				<div class="panel panel-primary">
+					<div class="panel-heading">
+						<h4 class="panel-title panelHeader">Applications</h4>
+					</div> <!-- End panel-heading -->
+					<div class="panel-body">
+							<?php
+								$applications = Application::getApplications(null,null,$term,PENDING);
+								if(!$applications){
+							?>
+									<div class="row">
+										<div class="col-xs-6">
+											<div class="alert alert-danger" role="alert">
+												<p>There are currently no available applications for this term.</p>
+											</div>											
+										</div> <!-- End column -->
+									</div> <!-- End row -->
+							<?php		
+								}else{
+							?>
+
+									<table class="table table-striped">
+										<thead>
+											<tr>
+												<th>Name</th>
+												<th>University ID</th>
+												<th>Email</th>
+												<th>Type</th>
+												<th>Application</th>
+												<th>Reviews</th>
+											</tr>
+										</thead>
+										<?php
+
+										/* Insert each application */
+										foreach($applications as $application){
+											$student = $application->getCreator();
+											$position = $application->getPosition();
+											$profileID = "myProfile" . $student->getID();
 
 										?>
-										
 										<tr>
-										<td><?= $student->getUniversityID() ?></td>
-										<td><?= $student->getFirstName()?></td>
-										<td><?= $student->getLastName() ?></td>
-										<td><?= $student->getEmail() ?></td>
-										<td><?= $student->getGPA()?></td>
-										<td>
-										<button data-toggle="modal" data-target="#profile-modal" data-usertype="<?= STUDENT ?>" data-userid="<?= $student->getID() ?>" class="btn btn-default circle profile">
-										<span class="glyphicon glyphicon-user"></span></button>			
-										</td>
-										<td>
-										<button data-toggle="modal" data-target="#commentModal" data-commenterID="<?= $staff->getID() ?>" data-studentID="<?= $student->getID() ?>" class="btn btn-default comment">
-										<span class="glyphicon glyphicon-comment"></span></button>
-										</td>
-									</tr>
-									<?php
-									$tableEntry++;
-									}
+														<td>
+															<div class="dropdown actions">
+																<a class="dropdown-toggle" type="button" id="actionsMenu" data-toggle="dropdown">
+																<?= $student->getFirstName() . " " . $student->getLastName()?>
+																<span class="caret"></span>
+																</a>
+																<ul class="dropdown-menu" role="menu" id="actionsMenu" aria-labelledby="actionsMenu">
+																	<li role="presentation"><a class="comment" role="menuitem" data-commenterID="<?= $staff->getID() ?>" data-studentID="<?= $student->getID() ?>" data-toggle="modal" href="#commentModal" tabindex="-1">Review Student</a></li>
+																	<li role="presentation"><a data-toggle="modal" role="menuitem" tabindex="-1" data-target="#emailModal">Send Email</a></li>
+																</ul>
+															</div>
+														</td>
+											<td>
+												<?= $student->getUniversityID() ?>
+											</td>
+											<td>
+												<?= $student->getEmail() ?>
+											</td> 
+											<td>
+												<?= $position->getTypeTitle() ?>
+											</td>
+											<td>
+												<button data-toggle="modal" data-target="#profile-modal" data-appID="<?= $application->getID() ?>" data-usertype="<?= STUDENT ?>" data-userid="<?= $student->getID() ?>" class="btn btn-info circle profile">
+													<span class="glyphicon glyphicon-file"></span>
+												</button>
+											</td>
+											<td>
+												<button data-toggle="modal" data-target="#commentsModal" data-userID="<?= $student->getID() ?>" class="btn btn-info comments">
+													<span class="glyphicon glyphicon-comment"></span>
+												</button>
+											</td>
+										</tr> 												
+											
+											<?php
+											}
+											?>
+									</table> <!-- End table -->
+							<?php		
 								}
-								?>
-									
-								</table> <!-- End table -->
-							</div> <!-- End panel-body -->
-							<div class="panel-footer">
-								<div class="row">
-									<div class="col-xs-4">
-										<button name="applyDecision" id="applyDecisions" class="btn btn-success decisions"><span class="glyphicon glyphicon-ok-circle"></span> Submit Decisions</button>												
-									</div> <!-- End column -->
-								</div> <!-- End row -->
-							</div> <!-- End panel-footer -->									
-						</div> <!-- End panel panel-primary -->
-					</div> <!-- End container -->	
-				</div> <!-- End row -->	
+							?>
+					</div> <!-- End panel-body -->
+				</div> <!-- End panel panel-primary -->
 <?php
 }
 ?>

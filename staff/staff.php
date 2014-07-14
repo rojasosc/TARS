961de46ee9c33063c378567a85a94fd63d15aaae
Oscar Rojas <orojas@u.rochester.edu>
2014-07-13 20:40:14 -0400

    updated page to reflect design of professor.php

--- staff/staff.php
+++ staff/staff.php
@@ -48,15 +48,46 @@ if ($error != null) {
 }
 if ($error == null || $error->getAction() != Event::SESSION_CONTINUE) {
 ?>
+
 				<div class="container">
-					<div class="jumbotron">
-						<h2 class="welcome">Welcome <?= $staff->getFirstName() ?>!</h2>					
-						<h3><span class="glyphicon glyphicon-warning-sign"></span> Notifications</h3> 
-							<p>You have <?= $totalUnverified ?> <a href="reviewStudents.php">students</a> that need to be verified.</p>
-							<p>(name) has dropped out of his assistantship.</p>
-							<p>(name) has replaced (name) in (course) as a (type).</p>
-					</div> <!-- End jumbotron -->
+					<div class="panel panel-primary">
+						<div class="panel-heading">
+							<h4 class="panel-title panelHeader">Home</h4>
+						</div> <!-- End panel-heading -->
+						<div class="panel-body">
+								<div class="row">
+									<div class="col-xs-6">
+										<div class="row">
+											<div class="col-xs-12">
+												<h3>Announcements</h3>
+											</div> <!-- End column -->
+										</div> <!-- End row --> 								
+									    <div class="row">
+									    	<div class="col-xs-12">
+													<p>Remember to review <a href="reviewStudents.php">pending applications</a> to assist 
+														professors in making informed decisions.</p>							    			
+									    	</div> <!-- End column -->
+										</div> <!--End Row -->		
+									</div> <!-- End column -->
+									<div class="col-xs-6">
+										<div class="row">
+											<div class="col-xs-12">
+												<h3>Notifications</h3>
+											</div> <!-- End column -->
+										</div> <!-- End row -->
+										<div class="row">
+											<div class="col-xs-12">
+													<div class="alert alert-success" role="alert">
+															There are currently no notifications to display.
+													</div> <!-- End alert alert-danger -->
+											</div> <!-- End column -->									
+										</div> <!-- End row -->																
+									</div> <!-- End column -->									
+								</div> <!-- End row -->																			
+						</div> <!-- End panel-body -->
+					</div> <!-- End panel panel-primary -->
 				</div> <!-- End container -->
+
 <?php
 }
 ?>

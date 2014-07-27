70ac14266ee150bc94b78ce6dc8af1a0d3df11d7
Nate Book <nmbook@gmail.com>
2014-07-27 19:14:56 -0400

    Forgot the form target

---
+++ passrecov.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once 'db.php';
+require_once 'actions.php';
+
+$output = Action::callAction('passRecov', $_POST);
+
+session_start();
+$_SESSION['callbackResult'] = $output;
+
+header('Location: ./');
+

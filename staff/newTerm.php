175fb1bfd5c613d8fc6793b23e8902f821681de7
raign <jan2@u.rochester.edu>
2014-07-30 13:48:43 -0400

    new newTerm.php UI

--- staff/newTerm.php
+++ staff/newTerm.php
@@ -57,53 +57,43 @@ if ($error == null || $error->getAction() != Event::SESSION_CONTINUE) {
 							<p class="panelHeader">New Term</p>
 						</div> <!-- End panel-heading -->
 						<div class="panel-body">
-							<div class="container">
-								<div id="formBox">
-									<form enctype="multipart/form-data" action="../actions.php" class="form-horizontal" id="newTermForm" method="post">
-										<input type="hidden" name="MAX_FILE_SIZE" value="4000000" />
-										<p class="optionHeader">Upload Term</p>
-										<div class="row">
-													<div class="col-md-12">
-														<p>Use this form to upload a new term using a CSV file.
-														Once you have uploaded the file, you can make <a href="editTerm.php">modifications</a> to the new term</p>			
-													</div> <!-- End column -->						
-										</div> <!-- End row -->								
-										<div class="row">
-											<div class="col-md-2">
-												<div class="form-group">
-													<label class="control-label" for="termYear">Term Year</label>
-													<input type="text" id="termYear" name="termYear" class="form-control" />
-												</div> <!-- End form-group -->
-											</div> <!-- End column -->
-											<div class="col-md-2">
-												<div class="form-group">
-													<label class="control-label" for="termSemester">Term Semester</label>
-													<select id="termSemester" name="termSemester" class="selectpicker form-control">
+							<form enctype="multipart/form-data" action="../actions.php" class="form-horizontal" id="newTermForm" method="post">
+								<input type="hidden" name="MAX_FILE_SIZE" value="4000000" />
+								<p class="optionHeader">Upload Term</p>
+								<div class="row">
+									<div class="col-xs-12 col-sm-9 col-md-10">
+										<p>
+											Use this form to upload a new term using a CSV file.
+											Once you have uploaded the file, you can make <a href="editTerm.php">modifications</a> to the new term
+										</p>			
+									</div> <!-- End column -->						
+								</div> <!-- End row -->								
+								<div class="row">
+									<div class="col-xs-12 col-sm-6 col-md-2">
+										<label class="control-label" for="termYear">Term Year</label>
+										<input type="text" id="termYear" name="termYear" class="form-control" />
+									</div> <!-- End column -->
+									<div class="col-xs-12 col-sm-6 col-md-2">
+										<label class="control-label" for="termSemester">Term Semester</label>
+										<select id="termSemester" name="termSemester" class="selectpicker form-control">
 <?php
 $termSemesters = Term::getAllTermSemesters();
 foreach ($termSemesters as $termSemester) {
 	echo "<option value=\"$termSemester\">".ucfirst($termSemester).'</option>';
 }
 ?>
-													</select> <!-- End select -->										
-												</div> <!-- End form-group -->
-											</div> <!-- End column -->
-											<div class="col-md-2">
-												<div class="form-group"> 
-													<label class="control-label" for="termFile">Choose File</label><br>
-													<input type="file" title="Browse" id="termFile" name="termFile" data-filename-placement="inside">													
-												</div> <!-- End form-group -->							
-											</div>	<!-- End column -->
-											<div class="col-md-2">
-												<div class="form-group">
-													<button id="newTermButton" type="submit"  name="newTermButton" class="btn btn-success btn-block"><span class="glyphicon glyphicon-upload"></span> Upload Term</button>													
-												</div> <!-- End form-group -->							
-											</div>	<!-- End column -->												
-										</div> <!-- End row -->																					
-									</form> <!-- End form -->
-								</div> <!-- end formbox -->
-							</div> <!-- End container -->						
+										</select> <!-- End select -->				
+									</div> <!-- End column -->
+									<div class="col-xs-12 col-sm-12 col-md-2">
+										<label class="control-label" for="termFile">Choose File</label><br>
+										<input type="file" title="Browse" id="termFile" name="termFile" data-filename-placement="inside">								
+									</div>	<!-- End column -->
+								</div> <!-- End row -->						
+							</form> <!-- End form -->
 						</div> <!-- End panel-body -->
+						<div class="panel-footer">
+							<button form="newTermForm" id="newTermButton" type="submit"  name="newTermButton" class="btn btn-success btn-block"><span class="glyphicon glyphicon-upload"></span> Upload Term</button>				
+						</div>
 					</div> <!-- End panel panel-success -->						
 				</div> <!-- End container -->
 <?php
